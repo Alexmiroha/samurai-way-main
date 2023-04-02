92009fa8407db98ee8fa8f69c639376e9e98b299
AlexPC <pssxopatik673@gmail.com>
2023-04-02 22:38:00 +0200

    L8 was added some new tests

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,14 +1,17 @@
 import {TodoListType} from "../App";
+import { v1 } from "uuid";
 
 type ActionType = {
     type: string,
     [key: string]: any
 }
 
-export const todolistsReducer = (state: Array<TodoListType>, action: ActionType) => {
+export const todolistsReducer = (state: Array<TodoListType>, action: ActionType): Array<TodoListType> => {
     switch (action.type) {
-        case 'XXX':
-            return state
+        case 'REMOVE-TODOLIST':
+            return [...state.filter(tl => tl.id !== action.id)]
+        case 'ADD-TODOLIST':
+            return [...state, {id: v1(), title: action.title, filter: 'all'}]
         default:
             throw new Error('I dont understand this type')
     }
